7d56490affd33e036f8e8efd19e54cb5b7155e7a
KARISHMA <karishma1294@gmail.com>
2018-05-08 14:36:33 +0530

    Update mongodb-connect.js

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -2,7 +2,7 @@
 const {MongoClient,ObjectID}=require('mongodb');
 
 // var obj=new ObjectID();
-// console.log(obj);
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   if(err){
